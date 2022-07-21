a41a6c85137c1fd5707beb7de23e2c6393b7541e
EmirPlays <96060395+EmirPlays@users.noreply.github.com>
2022-07-21 13:49:58 +0800

    Add files via upload

---
+++ example_mods/scripts/credit's.lua
@@ -0,0 +1,11 @@
+function onCreate()
+makeLuaText('watermark', songName .. "Port by EmirPlays", 0, -2, 550); -- You can edit the created by Prisma Text just dont change anything else
+    setTextSize('watermark', 20);
+    addLuaText('watermark');
+end
+
+function onUpdate(elapsed)
+     if downscroll then
+          setProperty('watermark.y', 150)
+     end
+end
